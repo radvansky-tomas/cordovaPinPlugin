095a42da961474dc6283e586818c823fd46fc56b
Tomas Radvansky <radvansky.tomas@gmail.com>
2017-08-14 09:56:38 +0200

    Updated activity, plugin, broadcast receiver, web part

--- src/android/com/cordova/plugins/CordovaPinPlugin.java
+++ src/android/com/cordova/plugins/CordovaPinPlugin.java
@@ -10,34 +10,85 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 
 public class CordovaPinPlugin extends CordovaPlugin {
     private static final String TAG = "CordovaPinPlugin";
     private CordovaInterface cordova;
+    private CallbackContext callbackContext;
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         this.cordova = cordova;
         Log.d(TAG, "Initializing CordovaPinPlugin");
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("PIN");
+        cordova.getActivity().getApplicationContext().registerReceiver(receiver, filter);
+
     }
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
         Context context = cordova.getActivity().getApplicationContext();
+        this.callbackContext = callbackContext;
+
         if (action.equals("showPin")) {
-            this.openNewActivity(context);
+            this.openNewActivity(context, "Enter your pin", "Login", "Cancel");
             return true;
         }
+
         return false;
     }
 
-    private void openNewActivity(Context context) {
+    private void openNewActivity(Context context, String hint, String button1, String button2) {
         Intent intent = new Intent(context, CordovaPinPluginActivity.class);
-        this.cordova.getActivity().startActivity(intent);
+        if (hint != null) {
+            intent.putExtra("hint", hint);
+        }
+        if (button1 != null) {
+            intent.putExtra("button1", button1);
+        }
+        if (button2 != null) {
+            intent.putExtra("button2", button2);
+        }
+
+        this.cordova.getActivity().startActivityForResult(intent, 100);
     }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(cordova.getActivity(), "received", Toast.LENGTH_SHORT).show();
+            cordova.getActivity().finishActivity(100);
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    callbackContext.success(1);
+                }
+            }, 200);
+
+        }
+    };
+
+    public Bundle onSaveInstanceState()
+    {
+        Bundle state = new Bundle();
+        return state;
+    }
+
+    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext)
+    {
+        this.callbackContext = callbackContext;
+    }
+
 }
--- src/android/com/cordova/plugins/CordovaPinPluginActivity.java
+++ src/android/com/cordova/plugins/CordovaPinPluginActivity.java
@@ -1,14 +1,79 @@
 package com.cordova.plugins;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ionicframework.demo144774.R;
 
 public class CordovaPinPluginActivity extends Activity {
+
+    private EditText pinEditText;
+    private Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String package_name = getApplication().getPackageName();
         setContentView(getApplication().getResources().getIdentifier("cordova_pin_plugin_activity", "layout", package_name));
+
+        pinEditText = (EditText)findViewById(R.id.pinEditText);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+
+        Bundle b = this.getIntent().getExtras();
+        if (b != null)
+        {
+            String pinHint = b.getString("hint");
+            String button1Title = b.getString("button1");
+            String button2Title = b.getString("button2");
+
+            pinEditText.setHint(pinHint);
+            if (button1Title != null)
+            {
+                button1.setVisibility(View.VISIBLE);
+                button1.setText(button1Title);
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent broadcast = new Intent();
+                        broadcast.setAction("PIN");
+                        sendBroadcast(broadcast);
+                    }
+                });
+            }
+            else
+            {
+                button1.setVisibility(View.INVISIBLE);
+                button1.setText("");
+            }
+
+            if (button2Title != null)
+            {
+                button2.setVisibility(View.VISIBLE);
+                button2.setText(button2Title);
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent broadcast = new Intent();
+                        broadcast.setAction("PIN");
+                        sendBroadcast(broadcast);
+                    }
+                });
+            }
+            else
+            {
+                button2.setVisibility(View.INVISIBLE);
+                button2.setText("");
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Disable back
     }
 }
